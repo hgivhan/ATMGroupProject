00f4fa4b0cf4f7b186464c4538d0f8f5832b6fd6
christian <christian@zipcodes-MBP-51.lan>
2020-11-14 17:27:32 -0500

    worked on features for Accounts class

--- src/main/java/Investment.java
+++ src/main/java/Investment.java
@@ -1,2 +1,21 @@
 public class Investment extends Accounts{
+
+    public Investment(UsersAccountInfo userAccount, Users currentUser, Double balance) {
+        super(userAccount, currentUser, balance);
+    }
+
+    @Override
+    public void deposit(double depositAmount) {
+
+    }
+
+    @Override
+    public void withdraw(double withdrawAmount) {
+
+    }
+
+    @Override
+    public void transfer(double transferAmount, int transferToAccount) {
+
+    }
 }
--- src/main/java/Savings.java
+++ src/main/java/Savings.java
@@ -1,2 +1,21 @@
 public class Savings extends Accounts {
+
+    public Savings (UsersAccountInfo userAccount, Users currentUser, Double balance) {
+        super(userAccount, currentUser, balance);
+    }
+
+    @Override
+    public void deposit(double depositAmount) {
+
+    }
+
+    @Override
+    public void withdraw(double withdrawAmount) {
+
+    }
+
+    @Override
+    public void transfer(double transferAmount, int transferToAccount) {
+
+    }
 }
