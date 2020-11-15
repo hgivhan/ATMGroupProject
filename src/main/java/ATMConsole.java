e67fff68df4a31fb8404037ed70599503afcc9fe
Peter Kutchen <60986379+MrKutchen@users.noreply.github.com>
2020-11-15 15:39:15 -0500

    Update ATMConsole.java

--- src/main/java/ATMConsole.java
+++ src/main/java/ATMConsole.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 
 public class ATMConsole {
     private static boolean atmPowerOn = true;
+    private Scanner scanner;
+    private PrimaryUserOptionsMenu primaryUserOptionsMenu;
+    private UserManager userManager;
+
+    public ATMConsole(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
     public static boolean isAtmPowerOn() {
         return atmPowerOn;
@@ -12,8 +19,9 @@ public class ATMConsole {
         ATMConsole.atmPowerOn = atmPowerOn;
     }
 
-    public void runCLIMenu(Scanner scanner) {
+    public void runCLIMenu() {
         PrimaryUserOptionsMenu primaryUserOptionsMenu = new PrimaryUserOptionsMenu();
+        UserManager userManager = new UserManager();
 
         System.out.println("Welcome to C3's ATM.\n" +
                 "Please select from an option below.");
@@ -27,22 +35,28 @@ public class ATMConsole {
                     "Enter choice here: -> ");
 
             try {
-                int input = scanner.nextInt();
-                switch(input) {
-                    case 1:
-                        primaryUserOptionsMenu.runMainUserOptionsMenu(scanner);
-                        break;
-                    case 2:
-                        System.out.println("You created a new profile");
-                        break;
-                    case 3:
-                        System.out.println("You have canceled the transaction.\n" +
-                                "Enjoy the rest of your day.");
-                        atmPowerOn = false;
-                        break;
-                    default:
-                        System.out.println("\n" + "Incorrect option chosen, please choose one of the menu options below.");
-                        break;
+                try {
+                    int input = scanner.nextInt();
+                    switch (input) {
+                        case 1:
+                            userManager.switchUser();
+                            primaryUserOptionsMenu.runMainUserOptionsMenu();
+                            break;
+                        case 2:
+                            userManager.createNewUser();
+                            break;
+                        case 3:
+                            System.out.println("You have canceled the transaction.\n" +
+                                    "Enjoy the rest of your day.");
+                            atmPowerOn = false;
+                            break;
+                        default:
+                            System.out.println("\n" + "Incorrect option chosen, please choose one of the menu options below.");
+                            break;
+                    }
+                }catch (NullPointerException e) {
+                    System.out.println(("\n" + "Either you do not have an account with C3's ATM, or your username or password was incorrect.\n" +
+                            "Please try again or create an account then try again."));
                 }
             } catch (InputMismatchException e) {
                 scanner.next();
