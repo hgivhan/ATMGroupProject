5e9e811e86df4eb0ca999069d4d80564bb1ec0c6
Peter Kutchen <60986379+MrKutchen@users.noreply.github.com>
2020-11-15 15:40:28 -0500

    Update Investment.java

--- src/main/java/Investment.java
+++ src/main/java/Investment.java
@@ -1,22 +1,96 @@
-public class Investment extends Accounts{
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-    public Investment(UserManager userAccount, Users currentUser, Double balance) {
-        super(userAccount, currentUser, balance);
+public class Investment extends Accounts {
+    private Scanner scanner;
+    public Investment(Scanner scanner, Users currentActiveUser, Double balance) {
+        super(scanner, currentActiveUser, balance);
     }
 
-    @Override
-    public void deposit(double depositAmount) {
-
+    public Investment(Users currentActiveUser) {
+        super(currentActiveUser);
     }
 
-    @Override
-    public void withdraw(double withdrawAmount) {
-
-    }
-
-    @Override
-    public void transfer(double transferAmount, int transferToAccount) {
-
+    public void investmentAccountOptions(Users currentActiveUser) {
+        boolean powerOn = true;
+        while (ATMConsole.isAtmPowerOn() && powerOn) {
+            System.out.println("\n" +
+                    "Please select from the following options:\n" +
+                    "1 - Deposit to Investment Account\n" +
+                    "2 - Withdraw from Investment Account\n" +
+                    "3 - Transfer from Investment Account\n" +
+                    "4 - Investment Account Balance\n" +
+                    "5 - Go Back to Account Options Menu\n" +
+                    "6 - Cancel Transaction" +
+                    "---------------------------------\n" +
+                    "Enter choice here: -> ");
+            try {
+                try {
+                    int input = scanner.nextInt();
+                    switch (input) {
+                        case 1:
+                            System.out.println("Please indicate deposit amount:");
+                            Double depositAmount = scanner.nextDouble();
+                            currentActiveUser.getInvestment().setBalance(currentActiveUser.getInvestment().getBalance() + depositAmount);
+                            currentActiveUser.getTransactionHistory().add("Deposit processed. New balance: $" + currentActiveUser.getInvestment().getBalance());
+                            break;
+                        case 2:
+                            System.out.println("Please indicate withdrawal amount:");
+                            Double withdrawalAmount = scanner.nextDouble();
+                            if (withdrawalAmount > currentActiveUser.getInvestment().getBalance()) {
+                                System.out.println("You have insufficient funds.\nPlease re-enter a withdrawal amount: ");
+                                withdrawalAmount = scanner.nextDouble();
+                                continue;
+                            } else {
+                                currentActiveUser.getInvestment().setBalance(currentActiveUser.getInvestment().getBalance() - withdrawalAmount);
+                            }
+                            currentActiveUser.getTransactionHistory().add("Withdrawal processed. New balance: $" + currentActiveUser.getInvestment().getBalance());
+                            break;
+                        case 3:
+                            System.out.println("Please indicate what account you would like to transfer too?\n" +
+                                    "1 - Savings, 2 - Checking\n" +
+                                    "Enter choice here: -> ");
+                            int transferInput = scanner.nextInt();
+                            System.out.println("Please indicate transfer amount:");
+                            Double transferAmount = scanner.nextDouble();
+                            if (transferAmount > currentActiveUser.getInvestment().getBalance()) {
+                                System.out.println("You have insufficient funds.\nPlease re-enter a transfer amount: ");
+                                transferAmount = scanner.nextDouble();
+                                continue;
+                            } else {
+                                currentActiveUser.getInvestment().setBalance(currentActiveUser.getInvestment().getBalance() - transferAmount);
+                            }
+                            if (transferInput == 1) {
+                                currentActiveUser.getSavings().setBalance(currentActiveUser.getSavings().getBalance() + transferAmount);
+                                currentActiveUser.getTransactionHistory().add("Transfer processed. New Savings balance: $" + currentActiveUser.getSavings().getBalance());
+                            } else {
+                                currentActiveUser.getChecking().setBalance(currentActiveUser.getChecking().getBalance() + transferAmount);
+                                currentActiveUser.getTransactionHistory().add("Transfer processed. New Checking balance: $" + currentActiveUser.getChecking().getBalance());
+                            }
+                            break;
+                        case 4:
+                            System.out.println("Current Investment Account Balance: $" + currentActiveUser.getInvestment().getBalance());
+                            break;
+                        case 5:
+                            System.out.println("Returning to Account Options Menu");
+                            powerOn = false;
+                            break;
+                        case 6:
+                            System.out.println("You have canceled the transaction.\n" +
+                                    "Enjoy the rest of your day.");
+                            powerOn = false;
+                            ATMConsole.setAtmPowerOn(false);
+                            break;
+                        default:
+                            System.out.println("\n" + "Incorrect option chosen, please choose one of the menu options below.");
+                            break;
+                    }
+                } catch (NullPointerException e) {
+                    System.out.println(("\n" + "You do not have that type of account, please open that type of account to access select again"));
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(("\n" + "Invalid selection, please choose again."));
+            }
+        }
     }
 }
-
