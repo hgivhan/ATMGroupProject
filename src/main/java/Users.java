e3fd1d6bb2d0b68078fd0627b8fde9dc6250ad5b
Peter Kutchen <60986379+MrKutchen@users.noreply.github.com>
2020-11-15 11:21:22 -0500

    Update Users.java

--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,42 +1,96 @@
-import java.util.ArrayList;
 import java.util.List;
+
 public class Users {
     private String username;
     private String password;
-    private List<Profile> profileList;
-    public Users(String name, String password) {
-        this.username = name;
-        this.password = password;
-        this.profileList = new ArrayList<>();
+    private Integer pinNumber;
+    private java.util.List<String> transactionHistory;
+    private Checking checking;
+    private Savings savings;
+    private Investment investment;
+
+    public Users(String username) {
+        this.username = username;
     }
-    public String getPassword() {
-        return this.password;
-    }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    public List<Profile> getProfileList() {
-        return profileList;
+
+    public Integer getPinNumber() {
+        return pinNumber;
     }
-    public void removeAccount(Profile profile) {
-        profileList.remove(profile);
+
+    public void setPinNumber(Integer pinNumber) {
+        this.pinNumber = pinNumber;
     }
-    public void addAccount(Profile profile) {
-        profileList.add(profile);
+
+    public List<String> getTransactionHistory() {
+        return transactionHistory;
     }
-    public Profile getAccount(Long enteredId) {
-        for (Profile profile : getProfileList()) {
-            Long currentId = profile.getId();
-            if (currentId.equals(enteredId)) {
-                return profile;
-            }
-        }
-        return null;
+
+    public void setTransactionHistory(List<String> transactionHistory) {
+        this.transactionHistory = transactionHistory;
     }
+
+    public Checking getChecking() {
+        return checking;
+    }
+
+    public void setChecking(Checking checking) {
+        this.checking = checking;
+    }
+
+    public Savings getSavings() {
+        return savings;
+    }
+
+    public void setSavings(Savings savings) {
+        this.savings = savings;
+    }
+
+    public Investment getInvestment() {
+        return investment;
+    }
+
+    public void setInvestment(Investment investment) {
+        this.investment = investment;
+    }
+
+    public Users(String username, String password, Integer pinNumber) {
+        this.username = username;
+        this.password = password;
+        this.pinNumber = pinNumber;
+    }
+
+    public Users() {
+    }
+
+//    public void removeAccount(Profile profile) {
+//        profileList.remove(profile);
+//    }
+//    public void addAccount(Profile profile) {
+//        profileList.add(profile);
+//    }
+//    public Profile getAccount(Long enteredId) {
+//        for (Profile profile : getProfileList()) {
+//            Long currentId = profile.getId();
+//            if (currentId.equals(enteredId)) {
+//                return profile;
+//            }
+//        }
+//        return null;
+//    }
 }
