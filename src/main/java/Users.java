57f1b7cd5b1fa791e3be93e71e6a89ff2589cd9b
Peter Kutchen <60986379+MrKutchen@users.noreply.github.com>
2020-11-15 23:06:29 -0500

    Update Users.java

--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,13 +1,64 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Users {
     private String username;
     private String password;
-    private Integer pinNumber;
-    private java.util.List<String> transactionHistory;
+    private java.util.List<String> transactionHistory = new ArrayList<>();
     private Checking checking;
     private Savings savings;
     private Investment investment;
+
+    public void changeUserProfiles(Users currentActiveUser) {
+        Scanner scanner = new Scanner(System.in);
+        boolean powerOn = true;
+        while (ATMConsole.isAtmPowerOn() && powerOn) {
+            System.out.println("\n" +
+                    "Please select from the following options:\n" +
+                    "1 - Change Username\n" +
+                    "2 - Change Password\n" +
+                    "3 - Go Back to Primary User Options Menu\n" +
+                    "4 - Cancel Transaction\n" +
+                    "---------------------------------\n" +
+                    "Enter choice here: -> ");
+
+            try {
+                int input = scanner.nextInt();
+                switch (input) {
+                    case 1:
+                        System.out.println("Please provide your new username: ");
+                        String newUsername = scanner.next();
+                        currentActiveUser.setUsername(newUsername);
+                        System.out.println("New username saved.");
+                        break;
+                    case 2:
+                        System.out.println("Please provide your new password: ");
+                        String newPassword = scanner.next();
+                        currentActiveUser.setUsername(newPassword);
+                        System.out.println("New password saved.");
+                        break;
+                    case 3:
+                        powerOn = false;
+                        break;
+                    case 4:
+                        System.out.println("You have canceled the transaction.\n" +
+                                "Enjoy the rest of your day.");
+                        powerOn = false;
+                        ATMConsole.setAtmPowerOn(false);
+                        break;
+                    default:
+                        System.out.println("\n" + "Incorrect option chosen, please choose one of the menu options below.");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                scanner.next();
+                System.out.println("\n" + "Incorrect option chosen, please choose one of the menu options below.");
+            }
+        }
+    }
+
 
     public Users(String username) {
         this.username = username;
@@ -27,14 +78,6 @@ public class Users {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public Integer getPinNumber() {
-        return pinNumber;
-    }
-
-    public void setPinNumber(Integer pinNumber) {
-        this.pinNumber = pinNumber;
     }
 
     public List<String> getTransactionHistory() {
@@ -69,28 +112,18 @@ public class Users {
         this.investment = investment;
     }
 
-    public Users(String username, String password, Integer pinNumber) {
+    public Users(String username, String password) {
         this.username = username;
         this.password = password;
-        this.pinNumber = pinNumber;
     }
 
     public Users() {
     }
 
-//    public void removeAccount(Profile profile) {
-//        profileList.remove(profile);
-//    }
-//    public void addAccount(Profile profile) {
-//        profileList.add(profile);
-//    }
-//    public Profile getAccount(Long enteredId) {
-//        for (Profile profile : getProfileList()) {
-//            Long currentId = profile.getId();
-//            if (currentId.equals(enteredId)) {
-//                return profile;
-//            }
-//        }
-//        return null;
-//    }
+    @Override
+    public String toString() {
+        return "Users{" +
+                "transactionHistory=" + transactionHistory +
+                "\n" + '}';
+    }
 }
