0639af1e422b7d930a43cf803de11fc161b1a918
Peter Kutchen <60986379+MrKutchen@users.noreply.github.com>
2020-11-16 09:05:28 -0500

    Update AccountTest.java

--- src/main/test/AccountTest.java
+++ src/main/test/AccountTest.java
@@ -13,6 +13,16 @@ public class AccountsTest {
     }
 
     @Test
+    public void testCheckingAccountFail() {
+        Users expectedCurrentActiveUser = new Users("Amanda", "ILoveJUnitTesting");
+        Double expectedBalance = 1213.0;
+        expectedCurrentActiveUser.setChecking(new Checking(0));
+        expectedCurrentActiveUser.getChecking().setBalance(10000.00);
+        Double actualBal = expectedCurrentActiveUser.getChecking().getBalance();
+        Assert.assertNotEquals(expectedBalance, actualBal);
+    }
+
+    @Test
     public void testSavingsAccount() {
         Users expectedCurrentActiveUser = new Users("Peter", "Kutchen3");
         Double expectedBalance = 10000.00;
@@ -20,6 +30,16 @@ public class AccountsTest {
         expectedCurrentActiveUser.getSavings().setBalance(expectedBalance);
         Double actualBal = expectedCurrentActiveUser.getSavings().getBalance();
         Assert.assertEquals(expectedBalance, actualBal);
+    }
+
+    @Test
+    public void testSavingsAccountFail() {
+        Users expectedCurrentActiveUser = new Users("Peter", "Kutchen3");
+        Double expectedBalance = 10000.00;
+        expectedCurrentActiveUser.setSavings(new Savings(0));
+        expectedCurrentActiveUser.getSavings().setBalance(500.0);
+        Double actualBal = expectedCurrentActiveUser.getSavings().getBalance();
+        Assert.assertNotEquals(expectedBalance, actualBal);
     }
 
     @Test
@@ -33,12 +53,31 @@ public class AccountsTest {
     }
 
     @Test
+    public void testInvestmentAccountFail() {
+        Users expectedCurrentActiveUser = new Users("Christian", "YouTasteLikeChicken");
+        Double expectedBalance = 450.0;
+        expectedCurrentActiveUser.setInvestment(new Investment(0));
+        expectedCurrentActiveUser.getInvestment().setBalance(5000.0);
+        Double actualBal = expectedCurrentActiveUser.getInvestment().getBalance();
+        Assert.assertNotEquals(expectedBalance, actualBal);
+    }
+
+    @Test
     public void testAccountBalance() {
         Accounts newAccount = new Accounts(0);
         Double expectedBal = 11.40;
         newAccount.setBalance(expectedBal);
         Double actualBal = newAccount.getBalance();
         Assert.assertEquals(expectedBal, actualBal);
+    }
+
+    @Test
+    public void testAccountBalanceFail() {
+        Accounts newAccount = new Accounts(0);
+        Double expectedBal = 11.40;
+        newAccount.setBalance(15.00);
+        Double actualBal = newAccount.getBalance();
+        Assert.assertNotEquals(expectedBal, actualBal);
     }
 
     @Test
@@ -51,6 +90,15 @@ public class AccountsTest {
     }
 
     @Test
+    public void testCheckingBalanceFail() {
+        Checking newAccount = new Checking(0);
+        Double expectedBal = 450.0;
+        newAccount.setBalance(2349.0);
+        Double actualBal = newAccount.getBalance();
+        Assert.assertNotEquals(expectedBal, actualBal);
+    }
+
+    @Test
     public void testSavingsBalance() {
         Savings newAccount = new Savings(0);
         Double expectedBal = 2040.00;
@@ -60,11 +108,29 @@ public class AccountsTest {
     }
 
     @Test
+    public void testSavingsBalanceFail() {
+        Savings newAccount = new Savings(0);
+        Double expectedBal = 2040.00;
+        newAccount.setBalance(234.0);
+        Double actualBal = newAccount.getBalance();
+        Assert.assertNotEquals(expectedBal, actualBal);
+    }
+
+    @Test
     public void testInvestmentBalance() {
         Investment newAccount = new Investment(0);
         Double expectedBal = 230.0;
         newAccount.setBalance(expectedBal);
         Double actualBal = newAccount.getBalance();
         Assert.assertEquals(expectedBal, actualBal);
+    }
+
+    @Test
+    public void testInvestmentBalanceFail() {
+        Investment newAccount = new Investment(0);
+        Double expectedBal = 230.0;
+        newAccount.setBalance(4599.0);
+        Double actualBal = newAccount.getBalance();
+        Assert.assertNotEquals(expectedBal, actualBal);
     }
 }
